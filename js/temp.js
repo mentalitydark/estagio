7a03f916d557de5443e0ec5e2575f1396f43220e
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2020-03-12 00:52:48 -0300

    Update temp.js

--- js/temp.js
+++ js/temp.js
@@ -1,4 +1,4 @@
-const url_temp1 = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+const url_temp1 = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 const url_temp2 = ',br&appid=c6ddb1e39b35c06f08d4359645428546';
 function tempo(nome,estado) {
     const local = `${nome},${estado}`;
