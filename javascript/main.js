92b9947686a9dc0aa1c74f9f1de0ea2c052b5b10
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2021-09-22 19:06:43 -0300

    Configs Update; Added Google ESLint

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.querySelector('#canvas');
+  const context = canvas.getContext('2d');
+  context.fillRect(350, 250, 50, 50);
 });
