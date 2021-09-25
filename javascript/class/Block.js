07509d6994a81472cb8fdb525fb45d5e8c60a751
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2021-09-24 22:09:43 -0300

    Fimd do dia 3. Class Block.js criada. Blocos de colisões que serão utilizados para criar o mapa.

---
+++ javascript/class/Block.js
@@ -0,0 +1,31 @@
+export default class Block {
+    constructor(x, y, width, height) {
+        this._x = x;
+        this._y = y;
+        this._width = width;
+        this._height = height;
+        this._color = "rgba(0,0,0,0.5)";
+        this._visible = true;
+    }
+    // Get
+    get x() { return this._x; }
+    get y() { return this._y; }
+    get width() { return this._width; }
+    get height() { return this._height; }
+    get color() { return this._color; }
+    get visible() { return this._visible; }
+    // Set
+    set visible(bool) { this._visible = bool; }
+
+    // Functions
+    draw(context) {
+        if(this._visible) {
+            context.fillStyle = this._color;
+            context.fillRect(this._x, this._y, this._width, this._height);
+        }
+    }
+    halfWidth() { return this._width/2; }
+    halfHeight() { return this._height/2; }
+    centerX() { return this._x + this.halfWidth(); }
+    centerY() { return this._y + this.halfHeight(); }
+}
