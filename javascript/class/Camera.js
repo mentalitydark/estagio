4562927a9dbd74b95994f63763f8c5f781696fcf
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2021-09-27 22:08:58 -0300

    Fim do dia 4. Class Camera.js criado. Câmera que centraliza o personagem no meio.

---
+++ javascript/class/Camera.js
@@ -0,0 +1,22 @@
+export default class Camera {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this._width = width;
+        this._height = height;
+    }
+    get width() { return this._width; }
+    get height() { return this._height; }
+    topBorder() {
+        return this.y + this._height*0.5;
+    }
+    rightBorder() {
+        return this.x + this._width*0.5;
+    }
+    bottomBorder() {
+        return this.y + this._height*0.5;
+    }
+    leftBorder() {
+        return this.x + this._width*0.5;
+    }
+}
