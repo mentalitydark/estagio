37e4a7ed860ad8aded8f4d8d7d4f635a4554a1d0
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2021-10-06 20:27:00 -0300

    Fim do dia 11. Menu de pause criado; Fonte externa; Organização das variáveis.

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,14 @@
 "use strict";
 import {GAME_update, GAME_render} from "./function/Playing.js";
 import {MENU_update, MENU_render} from "./function/StartMenu.js";
-import {PAUSED_update, PAUSED_render} from "./function/Paused.js";
+import {PAUSED_update, PAUSED_render, backgroundTrue} from "./function/Paused.js";
 import {addToLoad, loadEvent, loadedAssets, assetsToLoad} from "./function/LoadImgs.js";
 import Player from "./class/player.js";
 import Camera from "./class/Camera.js";
 import World from "./class/World.js";
 import {load} from "./function/IndexedDB.js";
 import {keyMap, changeKey} from "./util/KeyMap.js";
-import {Blocks} from "./util/Variables.js";
+import {Variables, changeVariable} from "./util/Variables.js";
 // -----------
 
 // const
@@ -17,18 +17,14 @@ const canvas = document.querySelector("#canvas");
 const context = canvas.getContext("2d");
 const world_bg = new Image();
 const sprites = new Image();
-const START_MENU = 1;
-const PAUSED = 2;
-const PLAYING = 3;
 // -----------
 
 // let
 // TODO Organizar em um arquivo separado
-let LOADING = true;
 let keys = {};
+let keysUp = {};
 let blocks_renderGame = [];
 let blocks_Collision = [];
-let gameState = 1;
 // -----------
 
 // Image Load
@@ -44,7 +40,7 @@ const player = new Player("Maou", sprites);
 const camera = new Camera((player.Position.x-canvas.width)/4, (player.Position.y-canvas.height)/4, canvas.width, canvas.height);
 const world = new World(2000, 1500, world_bg);
 // TODO Organizar em um arquivo separado
-Blocks.forEach( block => {
+Variables.Blocks.forEach( block => {
     blocks_renderGame.push(block);
     blocks_Collision.push(block);
 });
@@ -56,21 +52,23 @@ document.addEventListener("keydown", (event) => {
 });
 document.addEventListener("keyup", (event) => {
     keys[event.key] = false;
+    if(Variables.gameState === Variables.PAUSED)
+        keysUp[event.key] = true;
     switch (event.key) {
     case keyMap.Escape:
-        gameState = gameState === PLAYING ? PAUSED : PLAYING;
+        Variables.gameState === Variables.PLAYING ? changeVariable("gameState", Variables.PAUSED) : changeVariable("gameState", Variables.PLAYING);
         break;
     case keyMap.Enter:
-        gameState = PLAYING;
+        Variables.gameState = Variables.PLAYING;
         changeKey("Enter", null);
         load(player);
         break;
     case keyMap.p:
-        if(gameState === PLAYING)
+        if(Variables.gameState === Variables.PLAYING)
             keys["p"] = true;
         break;
     case keyMap.o:
-        if(gameState === PLAYING)
+        if(Variables.gameState === Variables.PLAYING)
             keys["o"] = true;
         break;
     }
@@ -84,20 +82,21 @@ context.imageSmoothingEnabled = false;
 context.scale(2, 2);
 function gameLoop(timeStamp) {
     window.requestAnimationFrame(gameLoop, canvas);
-    if(loadedAssets === assetsToLoad.length) LOADING = true;
-    if(LOADING) {
-        switch (gameState) {
-        case START_MENU:
+    if(loadedAssets === assetsToLoad.length) Variables.LOADING = true;
+    if(Variables.LOADING) {
+        switch (Variables.gameState) {
+        case Variables.START_MENU:
             MENU_update(context);
             MENU_render(context, canvas);
             break;
-        case PLAYING:
+        case Variables.PLAYING:
             GAME_update(player, keys, camera, blocks_Collision, world);
             GAME_render(timeStamp, context, world, player, camera, blocks_renderGame, canvas);
+            backgroundTrue();
             break;
-        case PAUSED:
-            PAUSED_update(context);
-            PAUSED_render(context, canvas);
+        case Variables.PAUSED:
+            PAUSED_update(keysUp, player);
+            PAUSED_render(context);
             break;
         }
     }
